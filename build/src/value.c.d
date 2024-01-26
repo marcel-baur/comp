d59b0c93726bf27cbade1f1b0e1265954a189cd8
Marcel Baur <marcel@baur.gg>
2024-01-26 18:58:44 +0100

    feat: values

---
+++ build/src/value.c.d
@@ -0,0 +1,4 @@
+build/./src/value.c.o: src/value.c src/memory.h src/common.h src/value.h
+src/memory.h:
+src/common.h:
+src/value.h:
---
+++ src/value.c
@@ -0,0 +1,23 @@
+#include "memory.h"
+#include "value.h"
+
+void init_value_array(ValueArray *array) {
+    array->count = 0;
+    array->capacity = 0;
+    array->values = NULL;
+}
+
+void write_value_array(ValueArray *array, Value val) {
+    if (array->capacity < array->count + 1) {
+        int oldCapacity = array->capacity;
+        array->capacity = GROW_CAPACITY(oldCapacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+    }
+    array->values[array->count] = val;
+    array->count++;
+}
+
+void free_value_array(ValueArray *array) {
+    FREE_ARRAY(Value, array->values, array->capacity);
+    init_value_array(array);
+}
